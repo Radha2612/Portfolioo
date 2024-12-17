ca3881e61da85944caf47fab19a3fc9c2299b205
Ravi Chandra <radhakottapalli2003@gmail.com>
2024-12-17 15:39:03 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developerskilled in creating dynamic, responsive web applications. Proficient in frontend and backend technologies, including HTML, CSS, JavaScript, React, Node.js, and databases. Experienced in building user-friendly interfaces, optimizing performance, and deploying scalable solutions. Passionate about coding, problem-solving, and delivering end-to-end web solutions.`;
+
+export const ABOUT_TEXT = `I am a dedicated and passionate Full Stack Developer with expertise in building responsive and dynamic web applications. Skilled in both frontend and backend technologies, I strive to deliver user-friendly, scalable, and efficient solutions. I enjoy problem-solving, coding, and creating impactful digital experiences.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECT = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Face recognition App",
+    image: project2,
+    description:
+      " Developing an enterprise-level solution with python, focusing on accuracy, scalability and security by using technologies like Python, OpenCV.",
+    technologies: ["Python", "OpenCV"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Weather Website",
+    image: project4,
+    description:
+      "Designed a weather website for accurate weather update and information",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+  {
+    title: "QR code generator App",
+    image: project2,
+    description:
+      " Developing an QR code generator with python, focusing on accuracy, scalability and security by using technologies like Python, OpenCV.",
+    technologies: ["Python", "OpenCV","web scraping tools"],
+  },
+];
+
+export const CONTACT = {
+  address: "Hyderabad, India ",
+  phoneNo: "9327819871 ",
+  email: "radhakottapalli2003@gmail.com",
+};
